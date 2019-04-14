7fecea6cd17443bbd2510396c4a1bd3befc33f90
Dominik Zogg <dominik.zogg@gmail.com>
2019-04-14 14:42:23 +0200

    do not load annoations

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,8 +4,4 @@ declare(strict_types=1);
 
 namespace App;
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
-
 $loader = require __DIR__.'/../vendor/autoload.php';
-
-AnnotationRegistry::registerLoader(array($loader, 'loadClass'));

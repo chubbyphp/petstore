9736244be66d4a90ea7948336d0e9895d0288a1c
Dominik Zogg <dominik.zogg@gmail.com>
2019-04-14 13:51:35 +0200

    make routing service provider more readable

--- app/ServiceProvider/RouterServiceProvider.php
+++ app/ServiceProvider/RouterServiceProvider.php
@@ -15,11 +15,10 @@ use App\Controller\Swagger\IndexController as SwaggerIndexController;
 use App\Controller\Swagger\YamlController as SwaggerYamlController;
 use App\Middleware\AcceptAndContentTypeMiddleware;
 use App\Model\Pet;
-use Chubbyphp\Framework\Middleware\LazyMiddleware;
-use Chubbyphp\Framework\RequestHandler\LazyRequestHandler;
+use Chubbyphp\Framework\Middleware\LazyMiddleware as LM;
+use Chubbyphp\Framework\RequestHandler\LazyRequestHandler as LRH;
 use Chubbyphp\Framework\Router\FastRoute\UrlGenerator;
 use Chubbyphp\Framework\Router\RouteCollection;
-use Chubbyphp\Framework\Router\RouteInterface;
 use Pimple\Container;
 use Pimple\Psr11\Container as PsrContainer;
 use Pimple\ServiceProviderInterface;
@@ -34,39 +33,22 @@ final class RouterServiceProvider implements ServiceProviderInterface
         $container[RouteCollection::class] = function () use ($container) {
             $psrContainer = new PsrContainer($container);
 
-            $acceptAndContentTypeMiddleware = new LazyMiddleware($psrContainer, AcceptAndContentTypeMiddleware::class);
+            $acceptAndContentTypeMiddleware = new LM($psrContainer, AcceptAndContentTypeMiddleware::class);
 
             return (new RouteCollection())
-                ->route('/', RouteInterface::GET, 'index',
-                    new LazyRequestHandler($psrContainer, IndexController::class)
-                )
+                ->get('/', 'index', new LRH($psrContainer, IndexController::class))
                 ->group('/api')
-                    ->route('', RouteInterface::GET, 'swagger_index',
-                        new LazyRequestHandler($psrContainer, SwaggerIndexController::class)
-                    )
-                    ->route('/swagger.yml', RouteInterface::GET, 'swagger_yml',
-                        new LazyRequestHandler($psrContainer, SwaggerYamlController::class)
-                    )
-                    ->route('/ping', RouteInterface::GET, 'ping',
-                        new LazyRequestHandler($psrContainer, PingController::class),
+                    ->get('', 'swagger_index', new LRH($psrContainer, SwaggerIndexController::class))
+                    ->get('/swagger.yml', 'swagger_yml', new LRH($psrContainer, SwaggerYamlController::class))
+                    ->get('/ping', 'ping', new LRH($psrContainer, PingController::class),
                         [$acceptAndContentTypeMiddleware]
                     )
                     ->group('/pets', [$acceptAndContentTypeMiddleware])
-                        ->route('', RouteInterface::GET, 'pet_list',
-                            new LazyRequestHandler($psrContainer, ListController::class.Pet::class)
-                        )
-                        ->route('', RouteInterface::POST, 'pet_create',
-                            new LazyRequestHandler($psrContainer, CreateController::class.Pet::class)
-                        )
-                        ->route('/{id}', RouteInterface::GET, 'pet_read',
-                            new LazyRequestHandler($psrContainer, ReadController::class.Pet::class)
-                        )
-                        ->route('/{id}', RouteInterface::PUT, 'pet_update',
-                            new LazyRequestHandler($psrContainer, UpdateController::class.Pet::class)
-                        )
-                        ->route('/{id}', RouteInterface::DELETE, 'pet_delete',
-                            new LazyRequestHandler($psrContainer, DeleteController::class.Pet::class)
-                        )
+                        ->get('', 'pet_list', new LRH($psrContainer, ListController::class.Pet::class))
+                        ->post('', 'pet_create', new LRH($psrContainer, CreateController::class.Pet::class))
+                        ->get('/{id}', 'pet_read', new LRH($psrContainer, ReadController::class.Pet::class))
+                        ->put('/{id}', 'pet_update', new LRH($psrContainer, UpdateController::class.Pet::class))
+                        ->delete('/{id}', 'pet_delete', new LRH($psrContainer, DeleteController::class.Pet::class))
                     ->end()
                 ->end()
             ;

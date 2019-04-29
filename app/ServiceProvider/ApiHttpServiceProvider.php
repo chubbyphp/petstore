6873e1ed739e93830d9a89024ff767da2205b3e9
Dominik Zogg <dominik.zogg@gmail.com>
2019-04-29 18:15:51 +0200

    get chack to zend-diactoros

--- app/ServiceProvider/ApiHttpServiceProvider.php
+++ app/ServiceProvider/ApiHttpServiceProvider.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace App\ServiceProvider;
 
 use App\ApiHttp\Factory\InvalidParametersFactory;
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Nyholm\Psr7\Response;
 use Pimple\Container;
 use Pimple\ServiceProviderInterface;
-use Psr\Http\Message\ResponseFactoryInterface;
-use Psr\Http\Message\ResponseInterface;
+use Zend\Diactoros\ResponseFactory;
+use Zend\Diactoros\StreamFactory;
 
 final class ApiHttpServiceProvider implements ServiceProviderInterface
 {
@@ -20,18 +18,11 @@ final class ApiHttpServiceProvider implements ServiceProviderInterface
     public function register(Container $container): void
     {
         $container['api-http.response.factory'] = function () {
-            // this dirty hack is needed, cause empty '' as a response argument results in '' on response
-            return new class() implements ResponseFactoryInterface
-            {
-                public function createResponse(int $code = 200, string $reasonPhrase = ''): ResponseInterface
-                {
-                    return new Response($code, [], null, '1.1', '' !== $reasonPhrase ? $reasonPhrase : null);
-                }
-            };
+            return new ResponseFactory();
         };
 
         $container['api-http.stream.factory'] = function () {
-            return new Psr17Factory();
+            return new StreamFactory();
         };
 
         $container[InvalidParametersFactory::class] = function () {
--- public/index_phpunit.php
+++ public/index_phpunit.php
@@ -2,8 +2,7 @@
 
 declare(strict_types=1);
 
-use Nyholm\Psr7\Factory\Psr17Factory;
-use Nyholm\Psr7Server\ServerRequestCreator;
+use Zend\Diactoros\ServerRequestFactory;
 
 /** @var Chubbyphp\Framework\Application $app */
 
@@ -11,8 +10,4 @@ $env = 'phpunit';
 
 $app = require __DIR__ . '/../app/app.php';
 
-$psr17Factory = new Psr17Factory();
-
-$creator = new ServerRequestCreator($psr17Factory, $psr17Factory, $psr17Factory, $psr17Factory);
-
-$app->send($app->handle($creator->fromGlobals()));
+$app->send($app->handle(ServerRequestFactory::fromGlobals()));

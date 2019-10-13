c0895ccf3a1d2f676bdb2781575ff508efeca361
Dominik Zogg <dominik.zogg@gmail.com>
2019-10-13 16:08:58 +0200

    add setup-dev to composer.json

--- setup-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bin/console dbal:database:drop --if-exists --force --env=dev
-bin/console dbal:database:create --env=dev
-bin/console orm:schema-tool:create --env=dev
-bin/console orm:validate-schema --env=dev
-
-bin/console config:clean-directories cache log

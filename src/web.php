fccc8e2a80e799181cbed2501a5b5a951a28ad20
Dominik Zogg <dominik.zogg@gmail.com>
2020-11-28 16:21:38 +0100

    drop error handler
    https://github.com/chubbyphp/chubbyphp-framework/commit/0baaca33a331a6ea38f76eca2856e22d7fb4ce67

--- src/web.php
+++ src/web.php
@@ -5,13 +5,10 @@ declare(strict_types=1);
 namespace App;
 
 use Chubbyphp\Framework\Application;
-use Chubbyphp\Framework\ErrorHandler;
 use Psr\Container\ContainerInterface;
 use Psr\Http\Server\MiddlewareInterface;
 
 require __DIR__.'/../vendor/autoload.php';
-
-set_error_handler([new ErrorHandler(), 'errorToException']);
 
 return static function (string $env) {
     /** @var ContainerInterface $container */

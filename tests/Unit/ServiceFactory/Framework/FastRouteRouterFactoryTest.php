2ab335585d9da53da8ffe02552f5c97917b20039
Dominik Zogg <dominik.zogg@hotmail.com>
2024-02-12 16:48:08 +0100

    Add more FastRouteRouterFactory tests

--- tests/Unit/ServiceFactory/Framework/FastRouteRouterFactoryTest.php
+++ tests/Unit/ServiceFactory/Framework/FastRouteRouterFactoryTest.php
@@ -20,17 +20,57 @@ final class FastRouteRouterFactoryTest extends TestCase
 {
     use MockByCallsTrait;
 
-    public function testInvoke(): void
+    public function testInvokeWithoutCache(): void
     {
         /** @var ContainerInterface $container */
         $container = $this->getMockByCalls(ContainerInterface::class, [
             Call::create('get')->with('config')->willReturn([
-                'fastroute' => ['cache' => sys_get_temp_dir().'/'.uniqid('fastroute-')],
+                'fastroute' => ['cache' => null],
             ]),
         ]);
 
         $factory = new FastRouteRouterFactory();
 
-        self::assertInstanceOf(FastRouteRouter::class, $factory($container));
+        /** @var FastRouteRouter $fastRouteRouter */
+        $fastRouteRouter = $factory($container);
+
+        self::assertInstanceOf(FastRouteRouter::class, $fastRouteRouter);
+
+        $getter = \Closure::bind(
+            fn ($property) => $this->{$property},
+            $fastRouteRouter,
+            $fastRouteRouter::class
+        );
+
+        self::assertFalse($getter('cacheEnabled'));
+        self::assertSame('data/cache/fastroute.php.cache', $getter('cacheFile'));
+    }
+
+    public function testInvokeWithCache(): void
+    {
+        $cachePath = sys_get_temp_dir().'/'.uniqid('fastroute-');
+
+        /** @var ContainerInterface $container */
+        $container = $this->getMockByCalls(ContainerInterface::class, [
+            Call::create('get')->with('config')->willReturn([
+                'fastroute' => ['cache' => $cachePath],
+            ]),
+        ]);
+
+        $factory = new FastRouteRouterFactory();
+
+        /** @var FastRouteRouter $fastRouteRouter */
+        $fastRouteRouter = $factory($container);
+
+        self::assertInstanceOf(FastRouteRouter::class, $fastRouteRouter);
+
+        $getter = \Closure::bind(
+            fn ($property) => $this->{$property},
+            $fastRouteRouter,
+            $fastRouteRouter::class
+        );
+
+        self::assertTrue($getter('cacheEnabled'));
+        self::assertSame($cachePath, $getter('cacheFile'));
     }
 }

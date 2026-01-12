b252a92d1e4ceaefeeb15d2a1ff17d3f31e3de30
Dominik Zogg <dominik.zogg@ikmail.com>
2026-01-12 20:25:08 +0100

    do not use root in postgres and use uri instead of standalone values

--- config/prod.php
+++ config/prod.php
@@ -80,6 +80,7 @@ use Slim\Interfaces\InvocationStrategyInterface;
 use Slim\Interfaces\RouteCollectorInterface;
 use Slim\Interfaces\RouteParserInterface;
 use Symfony\Component\Console\Command\Command;
+use Doctrine\DBAL\Tools\DsnParser;
 
 $rootDir = \realpath(__DIR__ . '/..');
 $cacheDir = $rootDir . '/var/cache/' . $env;
@@ -155,15 +156,7 @@ return [
             ],
         ],
         'dbal' => [
-            'connection' => [
-                'driver' => 'pdo_pgsql',
-                'charset' => 'utf8',
-                'user' => getenv('DATABASE_USER'),
-                'password' => getenv('DATABASE_PASS'),
-                'host' => getenv('DATABASE_HOST'),
-                'port' => getenv('DATABASE_PORT'),
-                'dbname' => getenv('DATABASE_NAME'),
-            ],
+            'connection' => (new DsnParser(['pgsql' => 'pdo_pgsql']))->parse(getenv('POSTGRES_URI')),
         ],
         'driver' => [
             'classMap' => [

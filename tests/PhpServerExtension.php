c8d573f146727b01540ead3f5014eeb9a136ec21
Dominik Zogg <dominik.zogg@ikmail.com>
2026-01-25 12:49:32 +0100

    do not drop database, only drop schema, and setup:phpunit is uselessm its logic is done in PhpServerExtension

--- tests/PhpServerExtension.php
+++ tests/PhpServerExtension.php
@@ -53,10 +53,10 @@ final class PhpServerExtension implements ExecutionStartedSubscriber, Extension
 
         echo 'initialize: start'.PHP_EOL;
 
-        passthru($consolePath.' dbal:database:drop --if-exists --force --env=phpunit');
-        passthru($consolePath.' dbal:database:create --env=phpunit');
-        passthru($consolePath.' orm:schema-tool:update --dump-sql --force --env=phpunit');
-
+        passthru($consolePath.' dbal:database:create --if-not-exists --env=phpunit');
+        passthru($consolePath.' orm:schema-tool:drop --force --env=phpunit');
+        passthru($consolePath.' orm:schema-tool:create --env=phpunit');
+        passthru($consolePath.' orm:validate-schema --env=phpunit');
         passthru($consolePath.' clean-directories cache log --env=phpunit');
 
         echo 'initialize: end'.PHP_EOL.PHP_EOL;

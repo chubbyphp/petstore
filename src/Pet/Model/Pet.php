c863afc3c4ea55940f493163c8d4bedf94edae1d
Dominik Zogg <dominik.zogg@ikmail.com>
2026-02-11 20:57:55 +0100

    array<int, => list<

--- src/Pet/Model/Pet.php
+++ src/Pet/Model/Pet.php
@@ -74,7 +74,7 @@ final class Pet implements ModelInterface
     }
 
     /**
-     * @param array<int, Vaccination> $vaccinations
+     * @param list<Vaccination> $vaccinations
      */
     public function setVaccinations(array $vaccinations): void
     {
@@ -90,7 +90,7 @@ final class Pet implements ModelInterface
     }
 
     /**
-     * @return array<int, Vaccination>
+     * @return list<Vaccination>
      */
     public function getVaccinations(): array
     {
